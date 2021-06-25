50e385c1c6456a3b552b558b42871af95b8a6ec2
southbell2 <77674296+southbell2@users.noreply.github.com>
2021-06-25 21:59:12 +0900

    Add files via upload

---
+++ combination.java
@@ -0,0 +1,33 @@
+public class Combination {
+    public static void main(String[] args) {
+        int n = 4;
+        int[] arr = {1, 2, 3, 4};
+        boolean[] visited = new boolean[n];
+
+        for (int i = 1; i <= n; i++) {
+            combination(arr, visited, 0, n, i);
+        }
+    }
+
+    static void combination(int[] arr, boolean[] visited, int start, int n, int r) {
+        if (r == 0) {
+            print(arr, visited, n);
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            visited[i] = true;
+            combination(arr, visited, i + 1, n, r - 1);
+            visited[i] = false;
+        }
+    }
+
+    static void print(int[] arr, boolean[] visited, int n) {
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+}
