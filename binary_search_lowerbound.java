ee3d99481167076db235496e77a4440cf20393f2
southbell2 <77674296+southbell2@users.noreply.github.com>
2021-07-12 18:28:20 +0900

    중복값이 있는 이진탐색에서 lower bound 구하기

--- binary_search_lowerbound.java
+++ binary_search_lowerbound.java
@@ -1,9 +1,9 @@
 //lower bound 구하기
 while (start < end) {
-	int mid = (start + end) / 2;
-            if(list.get(mid) < score) {
-                    start = mid + 1;
-            }else {
-                    end = mid;
-            }
+    int mid = (start + end) / 2;
+    if(list.get(mid) < score) {
+        start = mid + 1;
+    }else {
+        end = mid;
+    }
 }
