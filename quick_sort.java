af95d221b342622a48a7fb1968eec71a3381c7eb
southbell2 <77674296+southbell2@users.noreply.github.com>
2021-10-23 00:20:36 +0900

    퀵소트 구현

--- quick_sort.java
+++ quick_sort.java
@@ -5,6 +5,7 @@ public static void quickSort(int start, int end, int[] arr) {
     int left = start;
     int right = end;
     while (left < right) {
+        //left == right 이 될때까지 반복하는데 i <= left인 모든 i에 대하여 arr[i] <= pivot 을 만족하고 left < i 인 모든 i에 대하여 pivot < arr[i] 을 만족한다
         //right을 먼저 줄여줘야 한다 그래야 left == right 이 될때 arr[left] <= pivot을 만족
         while (pivot < arr[right] && left < right) {
             right--;
