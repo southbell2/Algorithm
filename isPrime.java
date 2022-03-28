a7c266500a3c4e4d6660bc686a116365389e631c
이종남 <77674296+southbell2@users.noreply.github.com>
2022-03-29 04:00:17 +0900

    소수인지 판별하는 알고리즘

---
+++ isPrime.java
@@ -0,0 +1,9 @@
+public static boolean isPrime(int number) {
+    if(number == 1) return false;
+
+    for (int i = 2; i <= (int) Math.sqrt(number); i++) {
+        if(number % i == 0) return false;
+    }
+
+    return true;
+}
