d429aba30ed90809391e5d782707f44ea7c57b34
이종남 <southbell2357@naver.com>
2024-03-05 15:43:11 +0900

    Update isPrime.java

--- isPrime.java
+++ isPrime.java
@@ -1,5 +1,5 @@
 public static boolean isPrime(int number) {
-    if(number == 1) return false;
+    if(number == 1 || number == 0) return false;
 
     for (int i = 2; i <= (int) Math.sqrt(number); i++) {
         if(number % i == 0) return false;
